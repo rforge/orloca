83303f155e141bfd6ac6aee0253d1f07b04a517f
mmarquez <mmarquez@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-01-27 17:19:12 +0000

    Change project page
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/orloca@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,7 +1,3 @@
-
-<!-- This is the project specific website template -->
-<!-- It can be changed as liked or replaced by other content -->
-
 <?php
 
 $domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
@@ -33,6 +29,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <!-- get project title  -->
 <!-- own website starts here, the following may be changed as you like -->
 
+<!... MMM
 <?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
 $contents = '';
 while (!feof($handle)) {
@@ -40,12 +37,19 @@ while (!feof($handle)) {
 }
 fclose($handle);
 echo $contents; } ?>
+MMM -->
 
-<!-- end of project description -->
+<h1>[EN] Welcome to orloca project!</h1>
 
-<p> No content added. </p>
+<p>ORLOCA (Operations Research LOCational Analysis) deals with facility location problems. The most known is the min-sum or Fermat-Weber location problem, which seeks for a point such that the weighted sum of the distances to the demand set are minimized.</p>
 
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+<p>More information in <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>project summary page</strong></a>.</p>
+
+<h1>[ES] ¡Bienvenido al proyecto orloca!</h1>
+
+<p>ORLOCA (Operations Research LOCational Analysis) aborda los problemas de localización. El problema de localizaci&oacute;n m&aacute;s conocido es el problema min-sum o de Fermat-Weber, que busca un punto tal que la suma ponderada de las distancias al conjunto de demanda sea minimizada.</p>
+
+<p>M&aacute; informaci&oacute;n en la <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>p&aacute;gina resumen del proyecto</strong></a>.</p>
 
 </body>
 </html>
